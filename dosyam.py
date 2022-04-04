c673bb2e8aeb6c2dd8412f811825b362a35d2fc2
Mahmut Edip Negiz <100766962+medipnegiz@users.noreply.github.com>
2022-04-04 09:36:43 +0200

    dosyam modülü eklendi

---
+++ dosyam.py
@@ -0,0 +1,10 @@
+
+a = int(input('Sayiyi giriniz: '))
+def fonksiyonum(a):
+    while True:
+        if a % 2 == 0:
+            return print(f'{a} sayinin karesi:', a**2)
+            break
+        else:
+            a = a + 1
+fonksiyonum(5)
